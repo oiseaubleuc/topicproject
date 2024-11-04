f3f894673bcd52c8f3ab8867a8f9bbef03b0af22
oiseaubleuc <147993651+oiseaubleuc@users.noreply.github.com>
2024-11-04 11:58:48 +0100

    failed, terug herbeginnen

--- settings.py
+++ settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bookings.apps.BookingsConfig'
+    'myapp',
 ]
 
 MIDDLEWARE = [
@@ -75,12 +75,6 @@ WSGI_APPLICATION = 'topicproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -118,7 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "myapp/../static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -135,7 +129,14 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'info@meadetailling.com'
-EMAIL_HOST_PASSWORD = 'MehdiEA3010'
-DEFAULT_FROM_EMAIL = 'info@meadetailling.com'
+EMAIL_HOST_USER = 'oiseaubleu899@gmail.com'
+EMAIL_HOST_PASSWORD = 'bhkm drfp hawb yuga'
+DEFAULT_FROM_EMAIL = 'Carwash Detailing <oiseaubleu899@gmail.com>'
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
